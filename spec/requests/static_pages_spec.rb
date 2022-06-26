9539de52d240a6e053b0e9cb36263924e94ab09f
Julio Vasquez <juliodevasquez@gmail.com>
2022-06-25 17:18:53 -0700

    rspec added + controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
+  describe "GET /" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
+
 
   describe "GET /about" do
     it "returns http success" do
