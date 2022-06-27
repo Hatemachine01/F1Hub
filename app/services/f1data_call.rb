6b451554fe08183910c994edbb4755c8dcb6da71
Julio Vasquez <juliodevasquez@gmail.com>
2022-06-27 00:43:35 -0700

    next race info

--- app/services/f1data_call.rb
+++ app/services/f1data_call.rb
@@ -9,7 +9,7 @@ require 'date'
 		data = JSON.parse(response)
 		@season_data = []
 		season_progress = 0
-		 data["MRData"]['RaceTable']['Races'].each do |race|
+		data["MRData"]['RaceTable']['Races'].each do |race|
 			@season_data << {
 				raceName: "#{race["raceName"]}",
 				raceDate: "#{race["date"]}",
@@ -17,8 +17,6 @@ require 'date'
 				raceStatus: (Date.parse(race["date"]) < Date.today ? "Finished" : nil),
 				raceURL: "#{race["url"]}" 
 			}
-			
-
 			if Date.parse(race["date"]) < Date.today
 				season_progress += 1
 			end
@@ -26,6 +24,46 @@ require 'date'
 		 end
 		@season_data
 	end
+
+	def self.next_race
+		url = 'https://ergast.com/api/f1/current.json'
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		data = JSON.parse(response)
+		season_progress = 1
+		data["MRData"]['RaceTable']['Races'].each do |race|
+			if Date.parse(race["date"]) < Date.today
+				season_progress += 1
+			end
+		end
+		next_race_data = "https://ergast.com/api/f1/2022/" + "#{season_progress}.json"
+		uri = URI(next_race_data)
+		response = Net::HTTP.get(uri)
+		data = JSON.parse(response)
+		next_race_info = []
+			data["MRData"]['RaceTable']['Races'].each do |race|
+				next_race_info << {
+				raceName: "#{race["raceName"]}",
+				raceDate: "#{race["date"]}",
+				raceTime: "#{race["time"]}",
+				raceFirstPracticeDate: "#{race["FirstPractice"]["date"]}",
+				raceFirstPracticeTime: "#{race["FirstPractice"]["time"]}",
+				raceSecondPracticeDate: "#{race["SecondPractice"]["date"]}",
+				raceSecondPracticeTime: "#{race["SecondPractice"]["time"]}",
+				raceThirdPracticeTime: "#{race["ThirdPractice"]["time"]}",
+				raceThirdPracticeDate: "#{race["ThirdPractice"]["date"]}",
+				raceQualifyingTime: "#{race["Qualifying"]["time"]}",
+				}
+			end
+		next_race_info
+	end	
+
+
+
+
+
+
+
 
 
 
--- spec/services/f1data_call_spec.rb
+++ spec/services/f1data_call_spec.rb
@@ -8,3 +8,10 @@ describe 'F1Data' do
 end
 
 
+describe 'F1Data' do
+  it "API call to get data for next race" do
+     expect(F1dataCall.next_race).to  be_kind_of(Array)
+  end
+end
+
+
