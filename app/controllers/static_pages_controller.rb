1b7944532169807421169b3d76eee919a8fbdd1b
Julio Vasquez <juliodevasquez@gmail.com>
2022-07-02 20:12:27 -0700

    adds seperate page for constructor standings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,21 +5,22 @@ class StaticPagesController < ApplicationController
   @data	= F1dataCall.season_standings
   @race_news = []
   url = 'https://www.f1technical.net/rss/news.xml'
-  URI.open(url) do |rss|
-    feed = RSS::Parser.parse(rss)
-    puts "Title: #{feed.channel.title}"
-    feed.items.each do |item|
-    	@race_news << {
-    		 	 title: "#{item.title}",
-       			 link: "#{item.link}",
-       			 pubDate: "#{item.pubDate}"
-    	}
-    end
-  @constructor_data = F1dataCall.constructor_standings
-     
-    
+	  URI.open(url) do |rss|
+	    feed = RSS::Parser.parse(rss)
+	    puts "Title: #{feed.channel.title}"
+	    feed.items.each do |item|
+	    	@race_news << {
+	    		 	 title: "#{item.title}",
+	       			 link: "#{item.link}",
+	       			 pubDate: "#{item.pubDate}"
+	    	}
+   	 	end 
+   	 end 
     @race_news
   end
+
+  def constructors
+  	@constructor_data = F1dataCall.constructor_standings
   end
 
   def calendar
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#home'
+  get '/constructor_standings', to: 'static_pages#constructors'
   get '/calendar', to: 'static_pages#calendar'
   get '/next', to: 'static_pages#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
